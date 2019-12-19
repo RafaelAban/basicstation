5470901fe84510c36ab9df89d14e821370350bfb
svc_jenkins <ovelazquez@semtech.com>
2019-12-19 12:34:45 -0500

    Release v5.4.3

--- examples/cups/sim.py
+++ examples/cups/sim.py
@@ -1,29 +1,7 @@
-# --- Revised 3-Clause BSD License ---
-# Copyright (C) 2016-2019, SEMTECH (International) AG.
-# All rights reserved.
+# Copyright (C) 2016-2019 Semtech (International) AG. All rights reserved.
 #
-# Redistribution and use in source and binary forms, with or without modification,
-# are permitted provided that the following conditions are met:
-#
-#     * Redistributions of source code must retain the above copyright notice,
-#       this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright notice,
-#       this list of conditions and the following disclaimer in the documentation
-#       and/or other materials provided with the distribution.
-#     * Neither the name of the copyright holder nor the names of its contributors
-#       may be used to endorse or promote products derived from this software
-#       without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL SEMTECH BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# This file is subject to the terms and conditions defined in file 'LICENSE',
+# which is part of this source code package.
 
 import os
 import sys
@@ -36,16 +14,11 @@ from asyncio import subprocess
 
 sys.path.append('../../pysys')
 import tcutils as tu
+import testutils as tstu
 import simutils as su
 
 import logging
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
-handler.setFormatter(logging.Formatter('%(asctime)s [%(name).8s:%(levelname).5s] %(message)s'))
-
 logger = logging.getLogger('cups-sim')
-logger.setLevel(logging.DEBUG)
-logger.addHandler(handler)
 
 station = None
 infos = None
@@ -189,6 +162,7 @@ def sigHandler(signum, frame):
     sys.stdout.flush()
     quit()
 
+tstu.setup_logging()
 signal.signal(signal.SIGINT, sigHandler)
 
 task = asyncio.ensure_future(test_start())
